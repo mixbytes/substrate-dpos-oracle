75c0b4a196dcac5f06cf80068056d64d32edfe5a
Mikhail <sadsnake@mixbytes.io>
2019-12-19 14:32:04 +0300

    Realize rereserve assets func

--- runtime/src/tablescore.rs
+++ runtime/src/tablescore.rs
@@ -1,9 +1,10 @@
-use rstd::collections::btree_map::BTreeMap;
+use rstd::collections::{btree_map::BTreeMap, btree_set::BTreeSet};
 
-use support::{decl_event, decl_module, decl_storage, Parameter};
+use support::{decl_event, decl_module, decl_storage, dispatch::Result, Parameter};
 
 use codec::{Decode, Encode};
-use rstd::vec::Vec;
+use core::cmp::{Ord, Ordering, PartialOrd};
+use rstd::prelude::*;
 use sr_primitives::traits::{
     //CheckedAdd,
     Member,
@@ -11,43 +12,60 @@ use sr_primitives::traits::{
     SimpleArithmetic,
 };
 use system::ensure_signed;
-
 pub trait Trait: assets::Trait
 {
     type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
 
-    type TargetType: Parameter + SimpleArithmetic;
+    type TargetType: Default + Parameter + SimpleArithmetic;
     type TableId: Parameter + Member + SimpleArithmetic + Default + Copy;
 }
 
-type Balance<T: Trait> = <T as assets::Trait>::Balance;
-type AssetId<T: Trait> = <T as assets::Trait>::AssetId;
-type AccountId<T: Trait> = <T as system::Trait>::AccountId;
+type Balance<T> = <T as assets::Trait>::Balance;
+type AssetId<T> = <T as assets::Trait>::AssetId;
+type AccountId<T> = <T as system::Trait>::AccountId;
 
-#[derive(Encode, Decode)]
+#[derive(Encode, Decode, Clone, Eq, PartialEq)]
 #[cfg_attr(feature = "std", derive(Debug))]
-struct Record<T: Trait>
+pub struct Record<T: Trait>
 {
     target: T::TargetType,
-    balances: BTreeMap<AccountId<T>, Balance<T>>,
+    balance: Balance<T>,
 }
 
-impl<T: Trait> Record<T>
+impl<T: Trait> Ord for Record<T>
 {
-    fn get_balance(&self) -> Balance<T>
+    fn cmp(&self, other: &Self) -> Ordering
     {
-        self.balances
-            .iter()
-            .fold(Balance::<T>::default(), |all, (_, balance)| *balance + all)
+        self.balance.cmp(&other.balance)
     }
 }
 
-#[derive(Decode, Encode)]
+impl<T: Trait> PartialOrd for Record<T>
+{
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering>
+    {
+        Some(self.cmp(&other))
+    }
+}
+
+impl<T: Trait> Default for Record<T>
+{
+    fn default() -> Self
+    {
+        Record {
+            target: T::TargetType::default(),
+            balance: Balance::<T>::default(),
+        }
+    }
+}
+
+#[derive(Decode, Encode, Eq, PartialEq, Clone)]
 #[cfg_attr(feature = "std", derive(Debug))]
 pub struct Table<T: Trait>
 {
     vote_asset: AssetId<T>,
-    scores: Vec<Record<T>>,
+    scores: BTreeSet<Record<T>>,
+    reserved: BTreeMap<AccountId<T>, Balance<T>>,
 }
 
 impl<T: Trait> Default for Table<T>
@@ -56,14 +74,15 @@ impl<T: Trait> Default for Table<T>
     {
         Table {
             vote_asset: AssetId::<T>::default(),
-            scores: Vec::new(),
+            scores: BTreeSet::new(),
+            reserved: BTreeMap::new(),
         }
     }
 }
 
 decl_storage! {
     trait Store for Module<T: Trait> as Tablescore {
-        Scores get(scores): map T::TableId => Table<T>;
+        pub Scores get(scores): map T::TableId => Table<T>;
         TableScoreIdSequence get(next_asset_id): T::TableId;
     }
 }
@@ -72,9 +91,12 @@ decl_module! {
     pub struct Module<T: Trait> for enum Call where origin: T::Origin {
         fn deposit_event() = default;
 
-        pub fn vote(origin, table_id: T::TableId, count: Balance<T>)
+        pub fn vote(origin, table_id: T::TableId, count: Balance<T>, target: T::TargetType) -> Result
         {
-            let who = ensure_signed(origin)?;
+            let voter = ensure_signed(origin)?;
+
+            Self::rereserve(&voter, &table_id, count)?;
+            unimplemented!()
         }
     }
 }
@@ -88,4 +110,39 @@ decl_event!(
     }
 );
 
-impl<T: Trait> Module<T> {}
+impl<T: Trait> Module<T> 
+{
+    fn rereserve(voter: &AccountId<T>, table_id: &T::TableId, count: Balance<T>) -> Result
+    {
+            let mut result: Result = Ok(());
+            Scores::<T>::mutate(table_id, |table| {
+                match table.reserved.get(voter)
+                {
+                    Some(reserved) =>
+                    {
+                        match reserved.cmp(&count)
+                        {
+                            Ordering::Greater =>
+                            {
+                                assets::Module::<T>::unreserve(&table.vote_asset, voter, *reserved - count);
+                            }
+                            Ordering::Less =>
+                            {
+                                result = assets::Module::<T>::reserve(&table.vote_asset, voter, count - *reserved);
+                            }
+                            _ => {}
+                        }
+                    }
+                    None =>
+                    {
+                        result = assets::Module::<T>::reserve(&table.vote_asset, voter, count);
+                    }
+                }
+
+                //table.reserved.replace(voter, count); ToDo Don't work
+                table.reserved.remove(voter);
+                table.reserved.insert(voter.clone(), count);
+            });
+            result
+    }
+}
